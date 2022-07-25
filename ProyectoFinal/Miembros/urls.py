9845a2552dca268cf0f42c2533f4fc3f9513e6b7
RocioColino <rcolino.tr@gmail.com>
2022-07-25 22:54:15 +0900

    Cree pagina de perfil

--- ProyectoFinal/Miembros/urls.py
+++ ProyectoFinal/Miembros/urls.py
@@ -1,9 +1,9 @@
 #from django.contrib import admin
 from django.urls import path
-from .views import UserRegisterView
+from .views import UserEditView, UserRegisterView
 
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
-    
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
 ]
