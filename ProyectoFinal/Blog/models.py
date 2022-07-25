a36b2647d79bee7d3b5c830c63f551bcf5af34e2
RocioColino <rcolino.tr@gmail.com>
2022-07-26 00:38:18 +0900

    Cree profile con foto y bio

--- ProyectoFinal/Blog/models.py
+++ ProyectoFinal/Blog/models.py
@@ -19,7 +19,13 @@ class Categoria(models.Model):
 class Perfil(models.Model):
     user=models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     bio=models.TextField()
-    
+    avatar=models.ImageField(null=True, blank=True, upload_to="images/perfil/")
+    website_url=models.CharField(max_length=255, null=True, blank=True)
+    facebook_url=models.CharField(max_length=255, null=True, blank=True)
+    instagram_url=models.CharField(max_length=255, null=True, blank=True)
+    twitter_url=models.CharField(max_length=255, null=True, blank=True)
+
+
     def __str__(self):
        return str(self.user)
 
